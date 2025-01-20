4854df0809424160a0ec962f48b21e80670ff0e9
ModStart <modstart@163.com>
2025-01-20 09:31:06 +0800

    新增：MacOS 打包使用官方证书

--- scripts/notarize.cjs
+++ scripts/notarize.cjs
@@ -1,18 +1,33 @@
-const {notarize} = require('electron-notarize');
+const {notarize} = require("@electron/notarize");
 
 exports.default = async function notarizing(context) {
+    const appName = context.packager.appInfo.productFilename;
     const {electronPlatformName, appOutDir} = context;
-
-    if (electronPlatformName !== 'darwin') {
+    // We skip notarization if the process is not running on MacOS and
+    // if the enviroment variable SKIP_NOTARIZE is set to `true`
+    // This is useful for local testing where notarization is useless
+    if (
+        electronPlatformName !== "darwin" ||
+        process.env.SKIP_NOTARIZE === "true"
+    ) {
+        console.log(`  • Skipping notarization`);
         return;
     }
 
-    const appName = context.packager.appInfo.productFilename;
+    // THIS MUST BE THE SAME AS THE `appId` property
+    // in your electron builder configuration
+    const appId = "FocusAny";
+
+    let appPath = `${appOutDir}/${appName}.app`;
+    let {APPLE_ID, APPLE_ID_PASSWORD, APPLE_TEAM_ID} = process.env;
+    console.log(`  • Notarizing ${appPath}`);
 
     return await notarize({
-        appBundleId: 'com.focusany',
-        appPath: `${appOutDir}/${appName}.app`,
-        appleId: 'your-apple-id@example.com',
-        appleIdPassword: 'your-app-specific-password',
+        tool: "notarytool",
+        appBundleId: appId,
+        appPath,
+        appleId: APPLE_ID,
+        appleIdPassword: APPLE_ID_PASSWORD,
+        teamId: APPLE_TEAM_ID,
     });
 };
