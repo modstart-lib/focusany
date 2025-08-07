ceffd1e8853caab1c549f8983a98f66e5bf74cc6
ModStart <modstart@163.com>
2025-08-07 10:13:13 +0800

    feat: code optimized

--- src/components/Setting/SettingAbout.vue
+++ src/components/Setting/SettingAbout.vue
@@ -8,7 +8,7 @@ const setting = useSettingStore();
 const licenseYear = new Date().getFullYear();
 
 const doOpenLog = async () => {
-    await window.$mapi.file.openPath(window.$mapi.log.root());
+    await window.$mapi.app.openPath(window.$mapi.log.root());
 };
 </script>
 
