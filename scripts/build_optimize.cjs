d333366d1ccc5bac839c5521e767b56b2915476e
ModStart <modstart@163.com>
2025-08-07 17:03:50 +0800

    feat: build optimized

--- scripts/build_optimize.cjs
+++ scripts/build_optimize.cjs
@@ -1,17 +1,56 @@
-const common = require('./common.cjs')
+const common = require("./common.cjs");
+
+console.log("BuildOptimize", {
+    name: common.platformName(),
+    arch: common.platformArch(),
+});
 
 exports.default = async function (context) {
-    console.log('BuildOptimize.output', JSON.stringify({
-        name: common.platformName(),
-        arch: common.platformArch(),
-        appOutDir: context.appOutDir,
-        outDir: context.outDir,
-        root: context.appOutDir
-    }))
-    common.listFiles(context.appOutDir, true).forEach((p) => {
-        // console.log('BuildOptimize.path', (p.isDir ? 'D:' : 'F:') + p.path);
-    })
-    // const localeDir = context.appOutDir + "/FocusAny.app/Contents/Frameworks/Electron Framework.framework/Versions/A/Resources/";
+    console.log("BuildOptimize.output", {
+        context: context,
+        root: context.appOutDir,
+    });
+    // copy extra electron/resources/extra/[name]-[arch] to extra
+    const platformName = common.platformName();
+    const platformArch = common.platformArch();
+    const name = platformName + "-" + platformArch;
+
+    const srcDir = `electron/resources/extra/${name}`;
+    let destDir = null;
+    if (platformName === 'osx') {
+        destDir = common.pathResolve(
+            context.appOutDir,
+            `${context.packager.appInfo.productFilename}.app`,
+            "Contents",
+            "Resources",
+            "extra",
+            name
+        );
+    } else if (platformName === 'win') {
+        destDir = common.pathResolve(context.appOutDir, "resources", "extra", name);
+    } else if (platformName === 'linux') {
+        destDir = common.pathResolve(context.appOutDir, "resources", "extra", name);
+    }
+
+    console.log("BuildOptimize.copy", {
+        platformName,
+        platformArch,
+        srcDir,
+        destDir,
+    });
+
+    if (srcDir && common.exists(srcDir)) {
+        console.log(`Copying from ${srcDir} to ${destDir}`);
+        common.copy(srcDir, destDir, true);
+        console.log(`Copy completed`);
+    } else {
+        console.log(`No matching source directory found for platform: ${platformName}-${platformArch}`);
+    }
+
+    // common.listFiles(context.appOutDir, true).forEach((p) => {
+    // console.log('BuildOptimize.path', (p.isDir ? 'D:' : 'F:') + p.path);
+    // })
+    // const localeDir = context.appOutDir + "/AigcPanel.app/Contents/Frameworks/Electron Framework.framework/Versions/A/Resources/";
     // console.log(`localeDir: ${localeDir}`);
     // fs.readdir(localeDir, function (err, files) {
     //     if (!(files && files.length)) {
