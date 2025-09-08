ec2b241241d30e1e3d9c4e2c8e024736197704f6
ModStart <modstart@163.com>
2025-09-08 15:28:53 +0800

    feat: misc request

--- src/declarations/type.d.ts
+++ src/declarations/type.d.ts
@@ -366,6 +366,14 @@ declare interface Window {
         misc: {
             getZipFileContent: (path: string, pathInZip: string) => Promise<string>;
             unzip: (zipPath: string, dest: string, option?: { process: Function }) => Promise<void>;
+            zip: (zipPath: string, sourceDir: string, option?: { end?: (archive: any) => void; }) => Promise<void>;
+            request: (option: {
+                url: string;
+                method?: "GET" | "POST";
+                responseType?: "json" | "text" | "arraybuffer";
+                headers?: any;
+                data?: any;
+            }) => Promise<any>;
         };
         manager: {
             getConfig: () => Promise<ConfigRecord>;
