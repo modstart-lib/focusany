268ef21a1f8e08ce07698ab424bccf2450937acd
ModStart <modstart@163.com>
2025-09-18 10:56:10 +0800

    feat: robotjs to nut.js

--- electron/mapi/manager/automation/index.ts
+++ electron/mapi/manager/automation/index.ts
@@ -1,9 +1,9 @@
+import {Button, Key, keyboard, mouse} from "@nut-tree-fork/nut-js";
 import {activeWindow, Result} from "get-windows";
 import {windowManager} from "node-window-manager";
-import {ActiveWindow} from "../../../../src/types/Manager";
-import robot, {Keys} from "@hurdlegroup/robotjs";
-import {Log} from "../../log/main";
 import {Window} from "node-window-manager/src/classes/window";
+import {ActiveWindow} from "../../../../src/types/Manager";
+import {Log} from "../../log/main";
 
 export const ManagerAutomation = {
     init() {
@@ -63,18 +63,109 @@ export const ManagerAutomation = {
         return win;
     },
     async typeString(text: string): Promise<void> {
-        robot.typeString(text);
+        await keyboard.type(text);
     },
-    async typeKey(key: Keys): Promise<void> {
-        robot.keyTap(key);
+    async typeKey(key: string): Promise<void> {
+        const keyMap: { [key: string]: Key } = {
+            'a': Key.A,
+            'b': Key.B,
+            'c': Key.C,
+            'd': Key.D,
+            'e': Key.E,
+            'f': Key.F,
+            'g': Key.G,
+            'h': Key.H,
+            'i': Key.I,
+            'j': Key.J,
+            'k': Key.K,
+            'l': Key.L,
+            'm': Key.M,
+            'n': Key.N,
+            'o': Key.O,
+            'p': Key.P,
+            'q': Key.Q,
+            'r': Key.R,
+            's': Key.S,
+            't': Key.T,
+            'u': Key.U,
+            'v': Key.V,
+            'w': Key.W,
+            'x': Key.X,
+            'y': Key.Y,
+            'z': Key.Z,
+            '0': Key.Num0,
+            '1': Key.Num1,
+            '2': Key.Num2,
+            '3': Key.Num3,
+            '4': Key.Num4,
+            '5': Key.Num5,
+            '6': Key.Num6,
+            '7': Key.Num7,
+            '8': Key.Num8,
+            '9': Key.Num9,
+            'space': Key.Space,
+            'enter': Key.Enter,
+            'tab': Key.Tab,
+            'backspace': Key.Backspace,
+            'delete': Key.Delete,
+            'escape': Key.Escape,
+            'shift': Key.LeftShift,
+            'control': Key.LeftControl,
+            'alt': Key.LeftAlt,
+            'command': Key.LeftSuper,
+            'left': Key.Left,
+            'right': Key.Right,
+            'up': Key.Up,
+            'down': Key.Down,
+            'f1': Key.F1,
+            'f2': Key.F2,
+            'f3': Key.F3,
+            'f4': Key.F4,
+            'f5': Key.F5,
+            'f6': Key.F6,
+            'f7': Key.F7,
+            'f8': Key.F8,
+            'f9': Key.F9,
+            'f10': Key.F10,
+            'f11': Key.F11,
+            'f12': Key.F12
+        };
+        const nutKey = keyMap[key.toLowerCase()];
+        if (nutKey) {
+            await keyboard.pressKey(nutKey);
+        }
     },
     async mouseToggle(type: 'down' | 'up', button: 'left' | 'right' | 'middle'): Promise<void> {
-        robot.mouseToggle(type, button);
+        const buttonMap: { [key: string]: Button } = {
+            'left': Button.LEFT,
+            'right': Button.RIGHT,
+            'middle': Button.MIDDLE
+        };
+        const nutButton = buttonMap[button];
+        if (nutButton) {
+            if (type === 'down') {
+                await mouse.pressButton(nutButton);
+            } else {
+                await mouse.releaseButton(nutButton);
+            }
+        }
     },
     async moveMouse(x: number, y: number): Promise<void> {
-        robot.moveMouse(x, y);
+        await mouse.setPosition({x, y});
     },
     async mouseClick(button: 'left' | 'right' | 'middle', double: boolean = false): Promise<void> {
-        robot.mouseClick(button, double);
+        const buttonMap: { [key: string]: Button } = {
+            'left': Button.LEFT,
+            'right': Button.RIGHT,
+            'middle': Button.MIDDLE
+        };
+        const nutButton = buttonMap[button];
+        if (nutButton) {
+            if (double) {
+                await mouse.doubleClick(nutButton);
+            } else {
+                await mouse.click(nutButton);
+            }
+        }
     }
 };
